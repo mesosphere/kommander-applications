81648e2f0e9d2f557eba11fc829825b7f58700ee
Max Jonas Werner <mail@makk.es>
2021-11-15 14:54:18 +0100

    feat: add tooling for updating Flux
    This change bootstraps a Make configuration for running targets in CI
    as well as a 'flux-update' target that updates the manifests of Flux
    to the latest Flux version.

---
+++ make/all.mk
@@ -0,0 +1,10 @@
+INCLUDE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(INCLUDE_DIR)make.mk
+include $(INCLUDE_DIR)shell.mk
+include $(INCLUDE_DIR)help.mk
+include $(INCLUDE_DIR)repo.mk
+include $(INCLUDE_DIR)ci.mk
+include $(INCLUDE_DIR)docker.mk
+include $(INCLUDE_DIR)flux.mk
+include $(INCLUDE_DIR)tools.mk
