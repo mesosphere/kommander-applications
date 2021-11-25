3de8ec0903c8f39e88726c4179871d00280ddb48
Mikołaj Baranowski <mikolajb@gmail.com>
2021-11-25 17:33:18 +0100

    feat: Provide release target
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    Signed-off-by: Grace Do <xgrace@gmail.com>

---
+++ make/release.mk
@@ -0,0 +1,9 @@
+S3_BUCKET ?= "downloads.mesosphere.io"
+S3_PATH ?= "dkp"
+S3_ACL ?= "bucket-owner-full-control"
+
+.PHONY: release
+release: ARCHIVE_NAME = kommander-applications_$(GIT_TAG).tar.gz
+release: install-tool.awscli
+	git archive --format "tar.gz" -o $(ARCHIVE_NAME) $(GIT_TAG)
+	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
