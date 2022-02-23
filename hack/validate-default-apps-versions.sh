a241f07e612d0f62e01f3550a5404e664c54f8e3
Mikołaj Baranowski <mikolajb@gmail.com>
2022-02-23 17:57:45 +0100

    feat: Add versions of default enterprise apps (kommander-licensing)
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- hack/validate-default-apps-versions.sh
+++ hack/validate-default-apps-versions.sh
@@ -3,13 +3,18 @@ set -euo pipefail
 
 latestKommanderVersion=$(find "services/kommander" -maxdepth 1 -type d -regextype sed -regex '.*/[0-9]\+.[0-9]\+.[0-9]\+' -printf "%f\n" | sort --version-sort | tail -1)
 
-readarray apps < <(yq -e '.data["values.yaml"]' "services/kommander/$latestKommanderVersion/defaults/cm.yaml" | yq -e '.attached.prerequisites.defaultApps')
+validate_apps_in_yaml_path() {
+  readarray apps < <(yq -e '.data["values.yaml"]' "services/kommander/$latestKommanderVersion/defaults/cm.yaml" | yq -e "$1")
 
-for app in "${apps[@]}"; do
-  name=$(echo "$app" | yq -e 'keys | .[0]')
-  version=$(echo "$app" | yq -e '.[]')
-  if [ ! -d "services/$name/$version" ]; then
-    echo "kommander is set to use $name in version $version which doesn't exist"
-    exit 1
-  fi
-done
+  for app in "${apps[@]}"; do
+    name=$(echo "$app" | yq -e 'keys | .[0]')
+    version=$(echo "$app" | yq -e '.[]')
+    if [ ! -d "services/$name/$version" ]; then
+      echo "app $name in version $version specified in $1 doesn't exist"
+      exit 1
+    fi
+  done
+}
+
+validate_apps_in_yaml_path ".attached.prerequisites.defaultApps"
+validate_apps_in_yaml_path ".kommander-licensing.defaultEnterpriseApps"
