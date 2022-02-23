7ad2a9d4e6f5dac923ab4547118718894e2b08e0
Mikołaj Baranowski <mikolajb@gmail.com>
2022-02-23 14:49:08 +0100

    chore: Adding Max' suggestion
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- hack/validate-default-apps-versions.sh
+++ hack/validate-default-apps-versions.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-latestKommanderVersion=$(find services/kommander -maxdepth 1 -type d | sort --version-sort | tail -1)
+latestKommanderVersion=$(find "services/kommander" -maxdepth 1 -type d -regextype sed -regex '.*/[0-9]\+.[0-9]\+.[0-9]\+' -printf "%f\n" | sort --version-sort | tail -1)
 
-readarray apps < <(yq -e '.data["values.yaml"]' "$latestKommanderVersion/defaults/cm.yaml" | yq -e '.attached.prerequisites.defaultApps')
+readarray apps < <(yq -e '.data["values.yaml"]' "services/kommander/$latestKommanderVersion/defaults/cm.yaml" | yq -e '.attached.prerequisites.defaultApps')
 
 for app in "${apps[@]}"; do
   name=$(echo "$app" | yq -e 'keys | .[0]')
