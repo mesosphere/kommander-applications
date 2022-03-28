c42dd8931b345ea9f085e861880d66376a2e543f
Jimmi Dyson <jimmidyson@gmail.com>
2022-03-28 13:14:35 +0100

    build: Add dir prefix in release tarball

--- make/release.mk
+++ make/release.mk
@@ -5,5 +5,7 @@ S3_ACL ?= "bucket-owner-full-control"
 .PHONY: release
 release: ARCHIVE_NAME = kommander-applications_$(GIT_TAG).tar.gz
 release: install-tool.awscli
-	git archive --format "tar.gz" -o $(ARCHIVE_NAME) $(GIT_TAG)
+	git archive --format "tar.gz" -o $(ARCHIVE_NAME) \
+	                              --prefix kommander-applications/ \
+	                              $(GIT_TAG)
 	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
