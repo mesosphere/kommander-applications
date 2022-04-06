42f8f06b67d1a5a32b12f893ee83c235d8a940ca
Casey Buto <cbuto@d2iq.com>
2022-04-06 17:52:30 -0400

    build: Remove prefix from git archive

--- make/release.mk
+++ make/release.mk
@@ -6,7 +6,6 @@ S3_ACL ?= "bucket-owner-full-control"
 release: ARCHIVE_NAME = kommander-applications-$(GIT_TAG).tar.gz
 release: install-tool.awscli
 	git archive --format "tar.gz" -o $(ARCHIVE_NAME) \
-	                              --prefix kommander-applications/ \
 	                              $(GIT_TAG) -- \
 	                              common services
 	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
