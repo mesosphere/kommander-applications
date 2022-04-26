8c69474c39c04638325c40466f3fcf88dde5a507
Farhan <farhan5900@gmail.com>
2022-04-26 14:24:06 +0530

    feat: notify in slack about repo tarball release (#366)
    Co-authored-by: Rishabh Bohra <rishabhbohra01@gmail.com>
    Co-authored-by: Max Jonas Werner <mwerner@d2iq.com>

--- make/release.mk
+++ make/release.mk
@@ -4,9 +4,14 @@ S3_ACL ?= "bucket-owner-full-control"
 
 .PHONY: release
 release: ARCHIVE_NAME = kommander-applications-$(GIT_TAG).tar.gz
+release: PUBLISHED_URL = https://downloads.d2iq.com/dkp/$(GIT_TAG)/$(ARCHIVE_NAME)
 release: install-tool.awscli
 	git archive --format "tar.gz" -o $(ARCHIVE_NAME) \
 	                              $(GIT_TAG) -- \
 	                              common services
 	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
-	echo "Published to https://downloads.d2iq.com/$(S3_PATH)/$(ARCHIVE_NAME)"
+	echo "Published to $(PUBLISHED_URL)"
+	# Make sure to set SLACK_WEBHOOK environment variable to webhook url for the below mentioned channel
+	curl -X POST -H 'Content-type: application/json' \
+	--data '{"channel":"#eng-shipit","blocks":[{"type":"header","text":{"type":"plain_text","text":":github: Kommander Applications Git Repo Tarball $(GIT_TAG) is out!","emoji":true}},{"type":"section","text":{"type":"mrkdwn","text":"$(PUBLISHED_URL)"}}]}' \
+	$(SLACK_WEBHOOK)
