27c9044575e16f5b09ed42aee028abfeab392a03
farhan5900 <farhan5900@gmail.com>
2022-04-28 00:02:52 +0530

    chore: skip notification if it is dev release

--- make/release.mk
+++ make/release.mk
@@ -11,7 +11,9 @@ release: install-tool.awscli
 	                              common services
 	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
 	echo "Published to $(PUBLISHED_URL)"
+ifeq (,$(findstring dev,$(GIT_TAG)))
 	# Make sure to set SLACK_WEBHOOK environment variable to webhook url for the below mentioned channel
 	curl -X POST -H 'Content-type: application/json' \
 	--data '{"channel":"#eng-shipit","blocks":[{"type":"header","text":{"type":"plain_text","text":":github: Kommander Applications Git Repo Tarball $(GIT_TAG) is out!","emoji":true}},{"type":"section","text":{"type":"mrkdwn","text":"$(PUBLISHED_URL)"}}]}' \
 	$(SLACK_WEBHOOK)
+endif
