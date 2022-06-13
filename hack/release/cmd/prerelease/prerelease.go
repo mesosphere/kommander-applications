0a09d12e6b70fe55165e37f268f59a63b1f8de67
Casey Buto <cbuto@d2iq.com>
2022-06-13 14:14:36 -0400

    feat: Add helper CLI tool for managing the kommander chart versions
    * feat: Add helper CLI tool for managing the kommander chart versions
    
    * chore: fix pre-commit
    
    * ci: Add golangci-lint
    
    * test: update error messages
    
    * refactor: update flag names/bump Go version

---
+++ hack/release/cmd/prerelease/prerelease.go
@@ -0,0 +1,101 @@
+package prerelease
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/drone/envsubst"
+	"github.com/spf13/cobra"
+)
+
+var Cmd *cobra.Command //nolint:gochecknoglobals // Cobra commands are global.
+
+const (
+	versionFlagName               = "version"
+	repoFlagName                  = "repo"
+	kommanderChartVersionTemplate = "${kommanderChartVersion:=%s}"
+
+	kommanderHelmReleasePathPattern        = "./services/kommander/*/kommander.yaml"
+	kommanderAppMgmtHelmReleasePathPattern = "./services/kommander-appmanagement/*/kommander-appmanagement.yaml"
+)
+
+func init() { //nolint:gochecknoinits // Initializing cobra application.
+	Cmd = &cobra.Command{
+		Use:   "pre-release",
+		Short: "Handles pre-release tasks for kommander-applications (i.e. updating Kommander chart versions)",
+		Args:  cobra.MaximumNArgs(0),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			chartVersion := Cmd.Flag(versionFlagName).Value.String()
+			kommanderApplicationsRepo := Cmd.Flag(repoFlagName).Value.String()
+			if _, err := os.Stat(kommanderApplicationsRepo); os.IsNotExist(err) {
+				return err
+			}
+			fullChartVersion := fmt.Sprintf(kommanderChartVersionTemplate, chartVersion)
+			err := updateChartVersions(kommanderApplicationsRepo, fullChartVersion)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "Updated Kommander chart version to %s", chartVersion)
+			return nil
+		},
+	}
+	Cmd.Flags().String(versionFlagName, "", "the new Kommander chart version")
+	Cmd.Flags().String(repoFlagName, "", "the path to the local kommander-applications repository to modify")
+
+	err := Cmd.MarkFlagRequired(versionFlagName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = Cmd.MarkFlagRequired(repoFlagName)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func updateChartVersions(kommanderApplicationsRepo, chartVersion string) error {
+	kommanderHelmReleasePaths := []string{kommanderHelmReleasePathPattern, kommanderAppMgmtHelmReleasePathPattern}
+	for _, helmReleasePath := range kommanderHelmReleasePaths {
+		// Find the HelmRelease
+		matches, err := filepath.Glob(filepath.Join(kommanderApplicationsRepo, helmReleasePath))
+		if err != nil {
+			return err
+		}
+		if len(matches) == 0 {
+			return fmt.Errorf("no matches found for HelmRelease path %s (verify the kommander-applications repo path is correct)", helmReleasePath)
+		}
+		if len(matches) > 1 {
+			return fmt.Errorf("found > 1 match for HelmRelease path %s (there should only be one match)", helmReleasePath)
+		}
+		helmReleaseFilePath := matches[0]
+
+		// Update the kommanderChartVersion value
+		parsedFile, err := envsubst.ParseFile(helmReleaseFilePath)
+		if err != nil {
+			return err
+		}
+		subVars := map[string]string{
+			"kommanderChartVersion": chartVersion,
+			"releaseNamespace":      "${releaseNamespace}",
+		}
+		updatedFile, err := parsedFile.Execute(func(s string) string {
+			return subVars[s]
+		})
+		if err != nil {
+			return err
+		}
+
+		if !strings.Contains(updatedFile, chartVersion) {
+			return fmt.Errorf("failed to update Kommander HelmRelease chart version")
+		}
+
+		err = os.WriteFile(helmReleaseFilePath, []byte(updatedFile), 0644)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
