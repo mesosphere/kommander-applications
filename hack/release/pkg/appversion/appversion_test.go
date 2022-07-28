404990904c66a3bc98ae811bfd93e8b9b7111b59
Mikołaj Baranowski <mikolajb@gmail.com>
2022-07-28 18:03:07 +0200

    feat: Change kommander apps variable names
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- hack/release/pkg/appversion/appversion_test.go
+++ hack/release/pkg/appversion/appversion_test.go
@@ -14,9 +14,7 @@ import (
 )
 
 func TestMove(t *testing.T) {
-	dir := t.TempDir()
-	cmd := exec.Command("git", "clone", strings.Repeat("../", 4), dir)
-	require.NoError(t, cmd.Run())
+	dir := fetchRepo(t)
 
 	newVersion := "0.99.99"
 	err := SetKommanderAppsVersion(context.Background(), dir, newVersion)
@@ -26,4 +24,60 @@ func TestMove(t *testing.T) {
 	assert.NoError(t, err)
 	_, err = os.Stat(filepath.Join(dir, constants.KommanderAppMgmtPath, newVersion))
 	assert.NoError(t, err)
+}
+
+func TestReplaceContent(t *testing.T) {
+	dir := fetchRepo(t)
+	changes, err := ReplaceContent(context.Background(), dir, "0.99.99")
+	require.NoError(t, err)
+	assert.Equal(t, 5, changes)
+}
+
+func fetchRepo(t *testing.T) string {
+	t.Helper()
+
+	dir := t.TempDir()
+	cmd := exec.Command("git", "clone", strings.Repeat("../", 4), dir)
+	require.NoError(t, cmd.Run())
+
+	return dir
+}
+
+func TestRegexps(t *testing.T) {
+	prefixIndex := varNames.SubexpIndex("prefix")
+	suffixIndex := varNames.SubexpIndex("suffix")
+
+	cases := []struct {
+		input  string
+		prefix string
+		suffix string
+	}{
+		{
+			input:  "kommander-0.4.0-d2iq-defaults",
+			prefix: "kommander-",
+			suffix: "-d2iq-defaults",
+		},
+		{
+			input:  "kommander-0.4.0-overrides",
+			prefix: "kommander-",
+			suffix: "-overrides",
+		},
+		{
+			input:  "kommander-appmanagement-0.4.0-d2iq-defaults",
+			prefix: "kommander-appmanagement-",
+			suffix: "-d2iq-defaults",
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.input, func(t *testing.T) {
+			result := varNames.FindStringSubmatch(c.input)
+			require.GreaterOrEqual(t, len(result), suffixIndex)
+			assert.Equal(t, c.prefix, result[prefixIndex])
+			assert.Equal(t, c.suffix, result[suffixIndex])
+
+			reconstructed := result[prefixIndex] + "0.4.0" + result[suffixIndex]
+			assert.Equal(t, c.input, reconstructed)
+		})
+	}
 }
