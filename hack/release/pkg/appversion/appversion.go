1b58ce4107f89e0305eb327f47a86e1b568d2ca2
Mikołaj Baranowski <mikolajb@gmail.com>
2022-08-04 08:43:02 +0200

    feat: Change kommander apps variable names (#515)
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- hack/release/pkg/appversion/appversion.go
+++ hack/release/pkg/appversion/appversion.go
@@ -1,18 +1,34 @@
 package appversion
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"errors"
 	"fmt"
+	"io"
+	"io/fs"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
+	"regexp"
+	"strings"
 
 	"github.com/mesosphere/kommander-applications/hack/release/pkg/constants"
 )
 
-var ErrVersionNotFound = errors.New("cannot detect existing kommander app version")
+var (
+	ErrVersionNotFound = errors.New("cannot detect existing kommander app version")
+
+	// kommander-0.4.0-d2iq-defaults
+	// kommander-0.4.0-overrides
+	varNames = regexp.MustCompile(
+		`(?P<prefix>kommander(-appmanagement)?-)` +
+			constants.SemverRegexp +
+			`(?P<suffix>(-d2iq-defaults)|(-overrides))`,
+	)
+)
 
 func SetKommanderAppsVersion(ctx context.Context, dir string, version string) error {
 	kommanderPath := filepath.Join(dir, constants.KommanderAppPath)
@@ -46,6 +62,82 @@ func SetKommanderAppsVersion(ctx context.Context, dir string, version string) er
 	return nil
 }
 
+// ReplaceContent replaces version inside files
+func ReplaceContent(ctx context.Context, dir, version string) (int, error) {
+	kommanderPath := filepath.Clean(constants.KommanderAppPath)
+
+	changes := 0
+	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, _ error) error {
+		relPath, err := filepath.Rel(dir, path)
+		if err != nil {
+			return err
+		}
+
+		if relPath == "." {
+			return nil
+		}
+
+		if d.IsDir() {
+			if !strings.HasPrefix(kommanderPath, relPath) && !strings.HasPrefix(relPath, kommanderPath) {
+				return filepath.SkipDir
+			}
+		}
+
+		if !d.Type().IsRegular() || filepath.Ext(d.Name()) != ".yaml" {
+			return nil
+		}
+
+		newChanges, err := replaceContentInFile(ctx, path, version)
+		changes += newChanges
+
+		return err
+	})
+
+	return changes, err
+}
+
+func replaceContentInFile(ctx context.Context, file string, version string) (int, error) {
+	f, err := os.Open(file)
+	if err != nil {
+		return 0, err
+	}
+	defer f.Close()
+
+	fscanner := bufio.NewScanner(f)
+	output := &bytes.Buffer{}
+
+	changes := 0
+	for fscanner.Scan() {
+		text := fscanner.Text()
+
+		newText := varNames.ReplaceAllStringFunc(text, func(match string) string {
+			newValue, ok := replaceInString(match, version)
+			if ok {
+				changes++
+				return newValue
+			}
+			return match
+		})
+
+		fmt.Fprintln(output, newText)
+	}
+
+	f.Close()
+
+	if changes > 0 {
+		f, err = os.Create(file)
+		if err != nil {
+			return 0, err
+		}
+		defer f.Close()
+
+		_, err = io.Copy(f, output)
+		return changes, err
+	}
+
+	return changes, nil
+}
+
 func move(ctx context.Context, dir, oldVersion, newVersion string) error {
 	cmd := exec.CommandContext(ctx,
 		"git",
@@ -62,4 +154,16 @@ func move(ctx context.Context, dir, oldVersion, newVersion string) error {
 	}
 
 	return nil
+}
+
+func replaceInString(input, replace string) (string, bool) {
+	prefixIndex := varNames.SubexpIndex("prefix")
+	suffixIndex := varNames.SubexpIndex("suffix")
+
+	match := varNames.FindStringSubmatch(input)
+	if len(match) < suffixIndex {
+		return "", false
+	}
+
+	return match[prefixIndex] + replace + match[suffixIndex], true
 }
