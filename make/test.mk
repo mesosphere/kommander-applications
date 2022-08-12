5231b5fab78aead9a3197184f9fdec3d649be177
Jimmi Dyson <jimmidyson@gmail.com>
2022-08-12 17:52:14 +0100

    build: Test upgrade on Kubernetes v1.23
    Installation of Kommander <=v2.3 fails on Kubernetes >=v1.24 so run
    upgrade tests on Kubernetes v1.23 until previous version of Kommander
    used in upgrade tests is >=v2.4.

--- make/test.mk
+++ make/test.mk
@@ -10,6 +10,12 @@ E2E_TIMEOUT       ?= 120m
 # by default with DKP.
 # See https://github.com/mesosphere/kind-docker-image-automation/ for the build repo.
 E2E_KINDEST_IMAGE ?= "mesosphere/kind-node:v1.24.3"
+
+# Kommander <=v2.3 does not install on Kubernetes >=v1.24 due to the introduction of
+# `LegacyServiceAccountTokenNoAutoGeneration` feature (enabled by default). This breaks self-attachment
+# and therefore the whole install process so we need to run the upgrade tests against an older version
+# of Kubernetes. This can be removed once the `UPGRADE_FROM_VERSION` below is Kommander >=v2.4.
+E2E_KINDEST_IMAGE_FOR_UPGRADE_TEST ?= "mesosphere/kind-node:v1.23.9"
 UPGRADE_FROM_VERSION ?= "v2.2.2-dev"
 
 # (aweris): This should be a temporary workaround for v2.3.0 development. If you're still see clone test in v2.4.0
@@ -43,9 +49,8 @@ test.e2e.upgrade.singlecluster: kommander-e2e ; $(info $(M) running end-to-end k
 	cd $(KOMMANDER_E2E_DIR) && \
 		E2E_TEST_PATH="feature/upgrade/suites/kind/singlecluster" \
 		E2E_TIMEOUT=$(E2E_TIMEOUT) \
-		E2E_KINDEST_IMAGE=$(E2E_KINDEST_IMAGE) \
+		E2E_KINDEST_IMAGE=$(E2E_KINDEST_IMAGE_FOR_UPGRADE_TEST) \
 		E2E_KOMMANDER_APPLICATIONS_REPOSITORY="github.com/mesosphere/kommander-applications.git?ref=$(UPGRADE_FROM_VERSION)" \
 		E2E_KOMMANDER_APPLICATIONS_REPOSITORY_TO_UPGRADE="github.com/mesosphere/kommander-applications.git?ref=$(GIT_COMMIT)" \
-		E2E_KINDEST_IMAGE=$(E2E_KINDEST_IMAGE) \
 		VERBOSE=$(VERBOSE) \
 		make test.e2e
