c03dfee1136266d9703e1fa1d7f322da18de98a9
Mikołaj Baranowski <mikolajb@gmail.com>
2022-10-04 17:09:23 +0200

    feat: Enable coveralls reporting (#682)
    * feat: Enable coveralls reporting
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * feat: Add badge
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- make/go.mk
+++ make/go.mk
@@ -1,7 +1,8 @@
 .PHONY: go-test
 go-test: go-lint
-go-test: install-tool.golang
-	cd hack/release && go test ./...
+go-test: install-tool.golang install-tool.go.goveralls
+	cd hack/release && go test -v -race -covermode=atomic -coverprofile=coverage.out ./...
+	cd hack/release && goveralls -coverprofile=coverage.out -service=github
 
 .PHONY: mod-tidy
 mod-tidy: install-tool.golang
