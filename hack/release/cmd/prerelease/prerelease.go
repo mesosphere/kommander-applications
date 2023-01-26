daf4409f14450ebe7ad773ca81c5b0d94255b78f
d2iq-mergebot <22822961+d2iq-mergebot@users.noreply.github.com>
2023-01-26 17:45:47 +0000

    feat: add capimate automation script (2.3) (#905)
    * refactor: Make chart version updater functionality public (#511)
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * chore: added capimate and automation script (#739)
    
    This adds a CAPImate tag to KApps. It also provides a script and a
    test that will override that tag pre release.
    
    Closes D2IQ-93749
    
    * fix: Use release dev version for capimate
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    Co-authored-by: Mikołaj Baranowski <mikolajb@gmail.com>
    Co-authored-by: Clinton Medbery <clintonmedbery@users.noreply.github.com>
    Co-authored-by: Grace Do <xgrace@gmail.com>

--- hack/release/cmd/prerelease/prerelease.go
+++ hack/release/cmd/prerelease/prerelease.go
@@ -4,22 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/drone/envsubst"
+	"github.com/mesosphere/kommander-applications/hack/release/pkg/chartversion"
+	"github.com/mesosphere/kommander-applications/hack/release/pkg/updatecapimate"
 	"github.com/spf13/cobra"
 )
 
 var Cmd *cobra.Command //nolint:gochecknoglobals // Cobra commands are global.
 
 const (
-	versionFlagName               = "version"
-	repoFlagName                  = "repo"
-	kommanderChartVersionTemplate = "${kommanderChartVersion:=%s}"
-
-	kommanderHelmReleasePathPattern        = "./services/kommander/*/kommander.yaml"
-	kommanderAppMgmtHelmReleasePathPattern = "./services/kommander-appmanagement/*/kommander-appmanagement.yaml"
+	versionFlagName = "version"
+	repoFlagName    = "repo"
 )
 
 func init() { //nolint:gochecknoinits // Initializing cobra application.
@@ -28,17 +23,23 @@ func init() { //nolint:gochecknoinits // Initializing cobra application.
 		Short: "Handles pre-release tasks for kommander-applications (i.e. updating Kommander chart versions)",
 		Args:  cobra.MaximumNArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			chartVersion := Cmd.Flag(versionFlagName).Value.String()
+			chartVersionString := Cmd.Flag(versionFlagName).Value.String()
 			kommanderApplicationsRepo := Cmd.Flag(repoFlagName).Value.String()
 			if _, err := os.Stat(kommanderApplicationsRepo); os.IsNotExist(err) {
 				return err
 			}
-			fullChartVersion := fmt.Sprintf(kommanderChartVersionTemplate, chartVersion)
-			err := updateChartVersions(kommanderApplicationsRepo, fullChartVersion)
+
+			err := chartversion.UpdateChartVersions(kommanderApplicationsRepo, chartVersionString)
 			if err != nil {
 				return err
 			}
-			fmt.Fprintf(cmd.OutOrStdout(), "Updated Kommander chart version to %s", chartVersion)
+			fmt.Fprintf(cmd.OutOrStdout(), "Updated Kommander chart version to %s", chartVersionString)
+
+			err = updatecapimate.UpdateCAPIMateVersion(kommanderApplicationsRepo, chartVersionString)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "Updated CAPIMate version to %s", chartVersionString)
 			return nil
 		},
 	}
@@ -54,48 +55,4 @@ func init() { //nolint:gochecknoinits // Initializing cobra application.
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func updateChartVersions(kommanderApplicationsRepo, chartVersion string) error {
-	kommanderHelmReleasePaths := []string{kommanderHelmReleasePathPattern, kommanderAppMgmtHelmReleasePathPattern}
-	for _, helmReleasePath := range kommanderHelmReleasePaths {
-		// Find the HelmRelease
-		matches, err := filepath.Glob(filepath.Join(kommanderApplicationsRepo, helmReleasePath))
-		if err != nil {
-			return err
-		}
-		if len(matches) == 0 {
-			return fmt.Errorf("no matches found for HelmRelease path %s (verify the kommander-applications repo path is correct)", helmReleasePath)
-		}
-		if len(matches) > 1 {
-			return fmt.Errorf("found > 1 match for HelmRelease path %s (there should only be one match)", helmReleasePath)
-		}
-		helmReleaseFilePath := matches[0]
-
-		// Update the kommanderChartVersion value
-		parsedFile, err := envsubst.ParseFile(helmReleaseFilePath)
-		if err != nil {
-			return err
-		}
-		subVars := map[string]string{
-			"kommanderChartVersion": chartVersion,
-			"releaseNamespace":      "${releaseNamespace}",
-		}
-		updatedFile, err := parsedFile.Execute(func(s string) string {
-			return subVars[s]
-		})
-		if err != nil {
-			return err
-		}
-
-		if !strings.Contains(updatedFile, chartVersion) {
-			return fmt.Errorf("failed to update Kommander HelmRelease chart version")
-		}
-
-		err = os.WriteFile(helmReleaseFilePath, []byte(updatedFile), 0644)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
 }
---
+++ hack/release/pkg/updatecapimate/updatecapimate_test.go
@@ -0,0 +1,58 @@
+package updatecapimate
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/mesosphere/kommander-applications/hack/release/pkg/constants"
+	cp "github.com/otiai10/copy"
+	"github.com/stretchr/testify/assert"
+)
+
+const rootDir = "../../../../"
+
+func TestUpdateCAPIMateVersionsSuccessfully(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "prerelease")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpDir)
+
+	// Make a copy of the current repo state to modify
+	err = cp.Copy(rootDir, tmpDir)
+	assert.Nil(t, err)
+
+	updateToVersion := "v1.0.0"
+	err = UpdateCAPIMateVersion(tmpDir, updateToVersion)
+	assert.Nil(t, err)
+
+	// Path to the file we want to update
+	pathToChart := filepath.Join(constants.KommanderAppPath, "*/*/cm.yaml")
+
+	beforeUpdateFiles, err := filepath.Glob(filepath.Join(rootDir, pathToChart))
+	assert.Nil(t, err)
+
+	// Get the tmp file that holds CAPIMate info
+	afterUpdateFiles, err := filepath.Glob(filepath.Join(tmpDir, pathToChart))
+	assert.Nil(t, err)
+
+	beforeFile, err := os.ReadFile(beforeUpdateFiles[0])
+	assert.Nil(t, err)
+
+	afterFile, err := os.ReadFile(afterUpdateFiles[0])
+	assert.Nil(t, err)
+
+	assert.Contains(t, string(beforeFile), "tag: v2.3.2-dev")
+	assert.NotContains(t, string(beforeFile), "tag: v1.0.0")
+	assert.Contains(t, string(afterFile), "tag: v1.0.0")
+	assert.NotContains(t, string(afterFile), "tag: v2.3.2-dev")
+
+}
+
+func TestUpdateCAPIMateVersionsFailsWhenItCannotFindCM(t *testing.T) {
+	tmpDir, err := os.MkdirTemp("", "prerelease")
+	assert.Nil(t, err)
+	defer os.RemoveAll(tmpDir)
+	updateToVersion := "v1.0.0"
+	err = UpdateCAPIMateVersion(tmpDir, updateToVersion)
+	assert.ErrorContains(t, err, "verify the kommander-applications repo path")
+}
