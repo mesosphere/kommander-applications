7da93d5904bfbae2d53918cbbc98188d95314cea
Grace Do <xgrace@gmail.com>
2023-01-30 18:30:26 -0800

    fix: Fix capimate image to be last stable (2.2) (#927)
    fix: Fix capimate image to be last stable

--- hack/release/pkg/constants/constants.go
+++ hack/release/pkg/constants/constants.go
@@ -3,7 +3,7 @@ package constants
 const (
 	KommanderAppPath       = "./services/kommander/"
 	KommanderAppMgmtPath   = "./services/kommander-appmanagement/"
-	CAPIMateDefaultVersion = "v2.2.4-dev"
+	CAPIMateDefaultVersion = "v2.2.3"
 
 	// SemverRegexp validates any semver (taken verbatim from semver specs).
 	SemverRegexp = `v?(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<prerelease>(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+(?P<buildmetadata>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?` //nolint:lll // it's not readable anyway
