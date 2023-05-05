cedeb34461e53aec415d5e3c1e19a9324dc8be98
Zinnia Gibson <32083003+sonofgibs@users.noreply.github.com>
2023-05-05 10:15:21 -0500

    fix: revert from personal setup

--- hack/validate-default-apps-versions.sh
+++ hack/validate-default-apps-versions.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-latestKommanderVersion=$(gfind "services/kommander" -maxdepth 1 -type d -regextype sed -regex '.*/[0-9]\+.[0-9]\+.[0-9]\+' -printf "%f\n" | sort --version-sort | tail -1)
+latestKommanderVersion=$(find "services/kommander" -maxdepth 1 -type d -regextype sed -regex '.*/[0-9]\+.[0-9]\+.[0-9]\+' -printf "%f\n" | sort --version-sort | tail -1)
 
 validate_apps_in_yaml_path() {
   readarray apps < <(yq -e '.data["values.yaml"]' "services/kommander/$latestKommanderVersion/defaults/cm.yaml" | yq -e "$1")
