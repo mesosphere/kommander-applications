67665893871e5854943b6d4d960784081e70c6a5
Casey Buto <cbuto@d2iq.com>
2023-08-01 16:33:19 -0400

    feat: Add pre-upgrade jobs to run helm-mapkubeapis (#1439)
    * feat(kubecost): Add pre-upgrade to run helm-mapkubeapi
    
    * feat(kubecost): Use kommander chart version for kubetools image tag
    
    * feat(kubecost): Use force true on pre-upgrade job
    
    * feat(logging-operator): Add pre-upgrade job to run helm-mapkubeapis
    
    * feat(gatekeeper): Add pre-upgrade job to run helm-mapkubeapis
    
    * fix(gatekeeper): update release name
    
    * fix(gatekeeper): add dependsOn in HR kustomization
    
    * feat: Update release script to update the kubetools image tag
    
    * chore: clean up todos
    
    * refactor: typo
    
    * fix: check if HR exists in pre-upgrade scripts
    
    ---------
    
    Co-authored-by: Grace Do <xgrace@gmail.com>

--- hack/release/pkg/chartversion/chartversion.go
+++ hack/release/pkg/chartversion/chartversion.go
@@ -7,19 +7,30 @@ import (
 	"strings"
 
 	"github.com/drone/envsubst"
+
 	"github.com/mesosphere/kommander-applications/hack/release/pkg/constants"
 )
 
-const kommanderChartVersionTemplate = "${kommanderChartVersion:=%s}"
+const (
+	kommanderChartVersionTemplate = "${kommanderChartVersion:=%s}"
+	preUpgradePath                = "*/pre-upgrade/pre-upgrade.yaml"
+)
 
 var (
 	kommanderHelmReleasePathPattern        = filepath.Join(constants.KommanderAppPath, "*/kommander.yaml")
 	kommanderAppMgmtHelmReleasePathPattern = filepath.Join(constants.KommanderAppMgmtPath, "*/kommander-appmanagement.yaml")
 	kommanderOperatorPath                  = "./common/kommander-operator/helmrelease.yaml"
-	filesContainingKommanderVersion        = []string{
+	kubecostPreUpgradePath                 = filepath.Join(constants.KubecostPath, preUpgradePath)
+	gatekeeperPreUpgradePath               = filepath.Join(constants.GatekeeperPath, preUpgradePath)
+	loggingOperatorPreUpgradePath          = filepath.Join(constants.LoggingOperatorPath, preUpgradePath)
+
+	filesContainingKommanderVersion = []string{
 		kommanderHelmReleasePathPattern,
 		kommanderAppMgmtHelmReleasePathPattern,
 		kommanderOperatorPath,
+		kubecostPreUpgradePath,
+		gatekeeperPreUpgradePath,
+		loggingOperatorPreUpgradePath,
 	}
 )
 
