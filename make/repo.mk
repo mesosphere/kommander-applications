bd31dc5e8f4c6838d92a6c5cc9c213abb2a99b9e
Shaeed Noordeen <94617056+shaeed-noordeen@users.noreply.github.com>
2023-08-31 14:28:47 +0100

    ci: migrate tc remaining builds to gha (#1465)
    * ci: migrate tc remaining builds to gha
    
    * ci: remove invalid action
    
    * ci: remove buildkit due to docker error
    
    * ci: edit dockerfile
    
    * ci: edit dockerfile
    
    * ci: rearrange workflow structure
    
    * ci: fix github org
    
    * ci: update workflows
    
    * ci: set ci env
    
    * ci: checkout head instead of merge commit
    
    * ci: make requested changes
    
    * ci: change asdf version
    
    * ci: add release
    
    * ci: remove nexus
    
    * ci: add permissions
    
    * ci: fix aws account info
    
    * ci: fix docker issue
    
    * ci: add git tag
    
    * ci: add slack webhook
    
    * ci: fix whitespaces
    
    * ci: remove e2e tests
    
    * ci: remove unused if statements
    
    * ci: create-dev-tag only runs on main
    
    * ci: moved lint to selfhosted
    
    * ci: fix whitespaces
    
    ---------
    
    Co-authored-by: Casey Buto <cbuto@d2iq.com>

--- make/repo.mk
+++ make/repo.mk
@@ -6,8 +6,8 @@ GIT_COMMIT := $(shell git rev-parse "HEAD^{commit}")
 export GIT_TAG ?= $(shell git describe --tags "$(GIT_COMMIT)^{commit}" --match v* --abbrev=0 2>/dev/null)
 export GIT_CURRENT_BRANCH := $(shell git rev-parse --abbrev-ref HEAD)
 
-export GITHUB_ORG ?= mesosphere
-export GITHUB_REPOSITORY ?= kommander-applications
+export GITHUB_ORG = mesosphere
+export GITHUB_REPOSITORY = kommander-applications
 
 ifneq ($(shell git status --porcelain 2>/dev/null; echo $$?), 0)
 	export GIT_TREE_STATE := dirty
