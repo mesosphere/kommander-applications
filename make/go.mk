9a68449e9a50da1dbae899b1fe5518231ac69616
Mikołaj Baranowski <mikolajb@gmail.com>
2023-09-08 12:18:32 +0200

    chore: Update coveralls setup
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- make/go.mk
+++ make/go.mk
@@ -1,8 +1,7 @@
 .PHONY: go-test
 go-test: go-lint
-go-test: install-tool.golang install-tool.go.goveralls
+go-test: install-tool.golang
 	cd hack/release && go test -v -race -covermode=atomic -coverprofile=coverage.out ./...
-	cd hack/release && goveralls -coverprofile=coverage.out -service=github
 
 .PHONY: mod-tidy
 mod-tidy: install-tool.golang
