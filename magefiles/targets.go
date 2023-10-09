dc644ad192b4eec22bf79920c19bc7839b78f189
Jabar Asadi <jasadi@d2iq.com>
2023-10-09 18:00:54 +0200

    chore: bump dagger version

--- magefiles/targets.go
+++ magefiles/targets.go
@@ -4,8 +4,8 @@ package main
 
 import (
 	// mage:import asdf
-	_ "github.com/mesosphere/daggers/mage/asdf"
+	_ "github.com/mesosphere/d2iq-daggers/catalog/asdf"
 
 	// mage:import lint
-	_ "github.com/mesosphere/daggers/mage/precommit"
+	_ "github.com/mesosphere/d2iq-daggers/catalog/precommit"
 )
