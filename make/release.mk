6c19dbc6ff8ebd6a991daef0259ff2ee552b8db2
msdolbey <dolbes@gmail.com>
2023-11-08 22:27:00 -0600

    fix: xxclude ai-nav info agent from the bundle

--- make/release.mk
+++ make/release.mk
@@ -7,10 +7,12 @@ release: ARCHIVE_NAME = kommander-applications-$(GIT_TAG).tar.gz
 release: PUBLISHED_URL = https://downloads.d2iq.com/dkp/$(GIT_TAG)/$(ARCHIVE_NAME)
 release: install-tool.awscli
 	# We don't want to have ai-navigator in airgapped bundle
+	# and we don't want to have ai-navigator-cluster-info-agent in airgapped bundle
 	# the connected customers download the k-apps from GitHub where it is still present
 	git archive --format "tar.gz" -o $(ARCHIVE_NAME) \
 								  $(GIT_TAG) -- \
 								  common services charts ":(exclude)services/ai-navigator-app"
+								  common services charts ":(exclude)services/ai-navigator-cluster-info-agent"
 	aws s3 cp --acl $(S3_ACL) $(ARCHIVE_NAME) s3://$(S3_BUCKET)/$(S3_PATH)/
 	echo "Published to $(PUBLISHED_URL)"
 ifeq (,$(findstring dev,$(GIT_TAG)))
