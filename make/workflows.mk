74fe20d5329b359af0f1f502c0401a322e20079b
Grace Do <xgrace@gmail.com>
2024-02-05 13:35:18 -0800

    ci(gha): Fix labeler workflow after tooling bump (#1910)
    * ci(gha): Fix labeler workflow after tooling bump
    
    * ci(gha): Update services in pr labeler list
    
    * ci(gha): Run make workflow-labeler-yaml-update
    
    * ci(gha): Run labeler on all prs not just automated bump prs
    
    * ci(gha): Run pr labeler only on opened action type

--- make/workflows.mk
+++ make/workflows.mk
@@ -1,10 +1,10 @@
 .PHONY: workflow-labeler-yaml-update
-# ls -d services/* | awk 'NR > 1' | sed p
-#   Output path to all services (e.g. "services/centralized-grafana"), printing each line twice
-# awk 'NR % 2 { print $$0 ":" } !(NR % 2) {print "  - " $$0 "/**";}'
-#   For every odd line, suffix with ":" (This is the label name to be applied)
-#   For every even line, format it like "  - services/centralized-grafana/**" (This is the changed file path for which to apply the label)
-# The output file is formatted as required by the labeler workflow.
+# ls -d services/* | awk 'NR > 1'
+#   Output path to all services (e.g. "services/centralized-grafana"), printing each line twice.
+# awk '{ print $$0 ":\n- changed-files:\n  - any-glob-to-any-file:\n    - " $$0 "/**" }'
+#   For every line (service), apply the required configuraton file structure for each match object.
+# Each service points to the changed path glob for which to apply the label.
+# The output file is formatted as required by the labeler workflow (https://github.com/actions/labeler).
 workflow-labeler-yaml-update: ## Updates .github/service-labeler.yaml for use with labeler GH action
 workflow-labeler-yaml-update: ; $(info $(M) updating .github/service-labeler.yaml with latest services)
-	ls -d services/* | awk 'NR > 1' | sed p | awk 'NR % 2 { print $$0 ":" } !(NR % 2) {print "  - " $$0 "/**";}' > .github/service-labeler.yaml
+	ls -d services/* | awk 'NR > 1' | awk '{ print $$0 ":\n- changed-files:\n  - any-glob-to-any-file:\n    - " $$0 "/**" }' > .github/service-labeler.yaml
