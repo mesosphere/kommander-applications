09ed77ad761e8b867397e0751a89111cfac70fa5
Grace Do <xgrace@gmail.com>
2024-03-14 06:48:31 -0700

    ci: Switch to devbox (#2005) (#2017)
    * ci: Switch to devbox (#2005)
    
    * ci: Switch to devbox (#1843)
    
    * ci: Switch to devbox
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * fix: Martin's comments
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * chore: Addressing Jimmi's comments
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * chore: Another round of Jimmi's comments
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * chore: Use devbox run
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    ---------
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * refactor(gha): Use token instead of ssh key (#1972)
    
    * ci(gha): Update backport action repo
    
    * fix(ci): Run make targets in all modules
    
    * ci: Use the same magefiles mod versions as the main
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * ci: Do not lint magefiles and apptests
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * ci: Satisfy linters
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    ---------
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    Co-authored-by: Mikołaj Baranowski <mikolajb@gmail.com>
    
    * fix(hack/release): go mod tidy
    
    ---------
    
    Signed-off-by: Mikołaj Baranowski <mikolajb@gmail.com>
    Co-authored-by: Mikołaj Baranowski <mikolajb@gmail.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,1 @@
 include make/all.mk
-
-# Versions for tools that are not managed by asdf.
-ASDF_VERSION=0.8.1
-DOCKER_VERSION=20.10.7
-
-# Inputs required to build the CI Docker image with a determinstic tag.
-CI_DOCKER_BUILD_ARGS=ASDF_VERSION=$(ASDF_VERSION)\
-		DOCKER_VERSION=$(DOCKER_VERSION)
-CI_DOCKER_EXTRA_FILES=.tool-versions
--- make/go.mk
+++ make/go.mk
@@ -1,13 +1,12 @@
 .PHONY: go-test
 go-test: go-lint
-go-test: install-tool.golang install-tool.go.goveralls
 	cd hack/release && go test -v -race -covermode=atomic -coverprofile=coverage.out ./...
-	cd hack/release && goveralls -coverprofile=coverage.out -service=github
 
 .PHONY: mod-tidy
-mod-tidy: install-tool.golang
+mod-tidy:
 	cd hack/release && go mod tidy
+	cd magefiles && go mod tidy
 
 .PHONY: go-lint
-go-lint: install-tool.golang install-tool.golangci-lint
+go-lint:
 	cd hack/release && golangci-lint run ./...
