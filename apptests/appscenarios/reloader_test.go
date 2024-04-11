4cba49653fe7b4dabeb7284524f496af6e578cd8
York Chen <york.chen@nutanix.com>
2024-04-11 12:08:56 -0400

    test: reloader upgrade

--- apptests/appscenarios/reloader_test.go
+++ apptests/appscenarios/reloader_test.go
@@ -17,14 +17,14 @@ import (
 	"sigs.k8s.io/yaml"
 )
 
-var _ = Describe("Reloader Install Test", Ordered, Label("reloader", "install"), func() {
+var (
+	r                 reloader
+	hr                *fluxhelmv2beta2.HelmRelease
+	deploymentList    *appsv1.DeploymentList
+	reloaderContainer corev1.Container
+)
 
-	var (
-		r                 reloader
-		hr                *fluxhelmv2beta2.HelmRelease
-		deploymentList    *appsv1.DeploymentList
-		reloaderContainer corev1.Container
-	)
+var _ = Describe("Reloader Install Test", Ordered, Label("reloader", "install"), func() {
 
 	It("should install successfully with default config", func() {
 		r = reloader{}
@@ -85,60 +85,128 @@ var _ = Describe("Reloader Install Test", Ordered, Label("reloader", "install"),
 	})
 
 	It("should reload the application", func() {
-		// create a configmap with the old nginx config
-		err := r.ApplyNginxConfigmap(ctx, env, "nginx-cm-old.yaml")
-		Expect(err).To(BeNil())
-
-		// deploy the nginx deployment
-		deploymentYAML, err := os.ReadFile("../testdata/reloader/nginx.yaml")
-		nginxDeployment := &appsv1.Deployment{}
-		err = yaml.Unmarshal(deploymentYAML, nginxDeployment)
-		nginxDeployment.SetNamespace(kommanderNamespace)
-		nginxDeployment.SetAnnotations(map[string]string{
-			"configmap.reloader.stakater.com/reload": nginxCMName,
-		})
-		err = k8sClient.Create(ctx, nginxDeployment)
-		Expect(err).To(BeNil())
-
-		Eventually(func() error {
-			err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(nginxDeployment), nginxDeployment)
-			if err != nil {
-				return err
-			}
-
-			for _, cond := range nginxDeployment.Status.Conditions {
-				if cond.Status == corev1.ConditionTrue &&
-					cond.Type == appsv1.DeploymentAvailable {
-					return nil
-				}
-			}
-			return fmt.Errorf("deployment not ready yet")
-		}).WithPolling(pollInterval).WithTimeout(5 * time.Minute).Should(Succeed())
-
-		// update the CM to break the deployment
-		err = r.ApplyNginxConfigmap(ctx, env, "nginx-cm-new.yaml")
-		Expect(err).To(BeNil())
-		time.Sleep(1 * time.Second)
-
-		// check if the deployment is updated and in a broken state
-		Consistently(func() error {
-			err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(nginxDeployment), nginxDeployment)
-			if err != nil {
-				return err
-			}
-			// after the nginx config update, the probe will fail thus breaks the deployment
-			if nginxDeployment.Status.UpdatedReplicas == 1 &&
-				nginxDeployment.Status.UnavailableReplicas == 1 {
-				return nil
-			}
-			return fmt.Errorf("expected the deployment in a broken state")
-		}, "5s").WithPolling(pollInterval).Should(Succeed())
+		reloaderTestReload(r)
 	})
 
 })
 
 var _ = Describe("Reloader Upgrade Test", Ordered, Label("reloader", "upgrade"), func() {
-	It("should return the name of the scenario", func() {
-		Fail("not implemented")
+	It("should install the previous version successfully", func() {
+		r = reloader{}
+		err := r.InstallPreviousVersion(ctx, env)
+		Expect(err).To(BeNil())
+
+		hr := &fluxhelmv2beta2.HelmRelease{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       fluxhelmv2beta2.HelmReleaseKind,
+				APIVersion: fluxhelmv2beta2.GroupVersion.Version,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      r.Name(),
+				Namespace: kommanderNamespace,
+			},
+		}
+
+		Eventually(func() error {
+			err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(hr), hr)
+			if err != nil {
+				return err
+			}
+
+			for _, cond := range hr.Status.Conditions {
+				if cond.Status == metav1.ConditionTrue &&
+					cond.Type == apimeta.ReadyCondition {
+					return nil
+				}
+			}
+			return fmt.Errorf("helm release not ready yet")
+		}).WithPolling(pollInterval).WithTimeout(5 * time.Minute).Should(Succeed())
+
+	})
+
+	It("should upgrade reloader successfully", func() {
+		// this is installing the latest version of the reloader
+		err := r.Install(ctx, env)
+		Expect(err).To(BeNil())
+
+		hr = &fluxhelmv2beta2.HelmRelease{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       fluxhelmv2beta2.HelmReleaseKind,
+				APIVersion: fluxhelmv2beta2.GroupVersion.Version,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      r.Name(),
+				Namespace: kommanderNamespace,
+			},
+		}
+
+		Eventually(func() error {
+			err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(hr), hr)
+			if err != nil {
+				return err
+			}
+
+			for _, cond := range hr.Status.Conditions {
+				if cond.Status == metav1.ConditionTrue &&
+					cond.Type == apimeta.ReadyCondition {
+					return nil
+				}
+			}
+			return fmt.Errorf("helm release not ready yet")
+		}).WithPolling(pollInterval).WithTimeout(5 * time.Minute).Should(Succeed())
+	})
+
+	It("should reload the application", func() {
+		reloaderTestReload(r)
 	})
 })
+
+func reloaderTestReload(r reloader) {
+	err := r.ApplyNginxConfigmap(ctx, env, "nginx-cm-old.yaml")
+	Expect(err).To(BeNil())
+
+	// deploy the nginx deployment
+	deploymentYAML, err := os.ReadFile("../testdata/reloader/nginx.yaml")
+	nginxDeployment := &appsv1.Deployment{}
+	err = yaml.Unmarshal(deploymentYAML, nginxDeployment)
+	nginxDeployment.SetNamespace(kommanderNamespace)
+	nginxDeployment.SetAnnotations(map[string]string{
+		"configmap.reloader.stakater.com/reload": nginxCMName,
+	})
+	err = k8sClient.Create(ctx, nginxDeployment)
+	Expect(err).To(BeNil())
+
+	Eventually(func() error {
+		err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(nginxDeployment), nginxDeployment)
+		if err != nil {
+			return err
+		}
+
+		for _, cond := range nginxDeployment.Status.Conditions {
+			if cond.Status == corev1.ConditionTrue &&
+				cond.Type == appsv1.DeploymentAvailable {
+				return nil
+			}
+		}
+		return fmt.Errorf("deployment not ready yet")
+	}).WithPolling(pollInterval).WithTimeout(5 * time.Minute).Should(Succeed())
+
+	// update the CM to break the deployment
+	err = r.ApplyNginxConfigmap(ctx, env, "nginx-cm-new.yaml")
+	Expect(err).To(BeNil())
+	time.Sleep(1 * time.Second)
+
+	// check if the deployment is updated and in a broken state
+	Consistently(func() error {
+		err = k8sClient.Get(ctx, ctrlClient.ObjectKeyFromObject(nginxDeployment), nginxDeployment)
+		if err != nil {
+			return err
+		}
+		// after the nginx config update, the probe will fail thus breaks the deployment
+		if nginxDeployment.Status.UpdatedReplicas == 1 &&
+			nginxDeployment.Status.UnavailableReplicas == 1 {
+			return nil
+		}
+		return fmt.Errorf("expected the deployment in a broken state")
+	}, "5s").WithPolling(pollInterval).Should(Succeed())
+}
