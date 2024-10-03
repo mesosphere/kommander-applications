15677be094e2217e5fa390b4611dea6bdc190fe4
Mikołaj Baranowski <mikolajb@gmail.com>
2024-10-03 16:30:58 +0200

    build: Unify release code in one place (#2679)

--- make/all.mk
+++ make/all.mk
@@ -7,7 +7,6 @@ include $(INCLUDE_DIR)repo.mk
 include $(INCLUDE_DIR)docker.mk
 include $(INCLUDE_DIR)flux.mk
 include $(INCLUDE_DIR)tools.mk
-include $(INCLUDE_DIR)release.mk
 include $(INCLUDE_DIR)pre-commit.mk
 include $(INCLUDE_DIR)validate.mk
 include $(INCLUDE_DIR)go.mk
