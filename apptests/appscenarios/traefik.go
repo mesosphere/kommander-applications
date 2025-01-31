9f74dc8a7145a1c3ce4ae15d7aa33948c76e8029
Martin Hrabovcin <martin.hrabovcin@nutanix.com>
2025-01-31 10:08:51 +0000

    fix: avoid adding same traefik override

--- apptests/appscenarios/traefik.go
+++ apptests/appscenarios/traefik.go
@@ -6,15 +6,8 @@ import (
 	"os"
 	"path/filepath"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/util/retry"
-	controllerruntime "sigs.k8s.io/controller-runtime"
-	ctrlClient "sigs.k8s.io/controller-runtime/pkg/client"
-
-	fluxhelmv2beta2 "github.com/fluxcd/helm-controller/api/v2beta2"
 	"github.com/mesosphere/kommander-applications/apptests/constants"
 	"github.com/mesosphere/kommander-applications/apptests/environment"
-	"github.com/mesosphere/kommander-applications/apptests/flux"
 )
 
 type traefik struct {
@@ -60,6 +53,12 @@ func (t traefik) install(ctx context.Context, env *environment.Env, appPath stri
 		return err
 	}
 
+	traefikCMName := "traefik-overrides"
+	err = t.applyTraefikOverrideCM(ctx, env, traefikCMName)
+	if err != nil {
+		return err
+	}
+
 	// apply the rest of kustomizations
 	traefikDir := filepath.Join(appPath, "traefik")
 	if _, err := os.Stat(traefikDir); !os.IsNotExist(err) {
@@ -96,53 +95,13 @@ func (t traefik) install(ctx context.Context, env *environment.Env, appPath stri
 				return err
 			}
 		}
-	} else {
-		// If the `traefik` directory doesn't exist, apply the default (root) kustomizations
-		err = env.ApplyKustomizations(ctx, appPath, map[string]string{
-			"releaseNamespace":   kommanderNamespace,
-			"workspaceNamespace": kommanderNamespace,
-		})
-		if err != nil {
-			return err
-		}
 	}
 
-	traefikCMName := "traefik-overrides"
-	err = t.applyTraefikOverrideCM(ctx, env, traefikCMName)
-	if err != nil {
-		return err
-	}
-
-	hr := &fluxhelmv2beta2.HelmRelease{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       fluxhelmv2beta2.HelmReleaseKind,
-			APIVersion: fluxhelmv2beta2.GroupVersion.Version,
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      constants.Traefik,
-			Namespace: kommanderNamespace,
-		},
-	}
-
-	genericClient, err := ctrlClient.New(env.K8sClient.Config(), ctrlClient.Options{
-		Scheme: flux.NewScheme(),
+	// If the `traefik` directory doesn't exist, apply the default (root) kustomizations
+	return env.ApplyKustomizations(ctx, appPath, map[string]string{
+		"releaseNamespace":   kommanderNamespace,
+		"workspaceNamespace": kommanderNamespace,
 	})
-	if err != nil {
-		return fmt.Errorf("could not create the generic client: %w", err)
-	}
-
-	err = retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		_, err = controllerruntime.CreateOrUpdate(ctx, genericClient, hr, func() error {
-			hr.Spec.ValuesFrom = append(hr.Spec.ValuesFrom, fluxhelmv2beta2.ValuesReference{
-				Kind: "ConfigMap",
-				Name: traefikCMName,
-			})
-			return nil
-		})
-		return err
-	})
-
-	return err
 }
 
 func (t traefik) applyTraefikOverrideCM(ctx context.Context, env *environment.Env, cmName string) error {
