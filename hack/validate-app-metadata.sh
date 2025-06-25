f3a6999eb2d0694ab4bc0d032f86b0e658cd2aa4
Grace Do <xgrace@gmail.com>
2025-06-25 13:26:04 -0700

    refactor: Rename services dir to applications (#3588)
    * ci: Fix pre-commit metadata check
    
    * feat: s/services/applications in tooling
    
    * feat: Copy services/ to applications/
    
    * refactor: Update services paths
    
    * build: Updated .github/app-labeler.yaml
    
    * feat: Pull in updates from main
    
    * feat: Remove services dir
    
    ---------
    
    Co-authored-by: d2iq-mergebot <ci-mergebot@d2iq.com>

--- hack/validate-app-metadata.sh
+++ hack/validate-app-metadata.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 IFS=$'\n\t'
 
 set +e
-MISSING_SERVICE_YAMLS="$(diff <(for x in services/*/*/metadata.yaml; do dirname "$x"; done | sort -u) <(printf "%s\n" services/*/* | grep -Ev '/README.md$') | grep -E '^>')"
+MISSING_SERVICE_YAMLS="$(diff <(for x in applications/*/*/metadata.yaml; do dirname "$x"; done | sort -u) <(printf "%s\n" applications/*/* | grep -Ev '/README.md$') | grep -E '^>')"
 readonly MISSING_SERVICE_YAMLS
 set -e
 
 if [ -n "${MISSING_SERVICE_YAMLS}" ]; then
-  printf "The following services have missing metadata.yaml files:\n\n%s\n" "${MISSING_SERVICE_YAMLS}"
+  printf "The following applications have missing metadata.yaml files:\n\n%s\n" "${MISSING_SERVICE_YAMLS}"
   exit 1
 fi
