47f362421f4ce48ac104a7f504ab93bcefb995b2
Martin Hrabovcin <martin.hrabovcin@nutanix.com>
2025-07-01 21:24:40 +0000

    fix: add ai-navigator-gateway to skip images list

--- make/validate.mk
+++ make/validate.mk
@@ -3,6 +3,8 @@ DKP_BLOODHOUND_BIN := $(LOCAL_DIR)/bin/dkp-bloodhound_v$(DKP_BLOODHOUND_VERSION)
 GOOS := $(shell go env GOOS)
 GOARCH := $(shell go env GOARCH)
 
+SKIP_APPLICATIONS ?= ai-navigator-gateway,ai-navigator-app,ai-navigator-cluster-info-agent,nkp-pulse-management,nkp-pulse-workspace
+
 $(DKP_BLOODHOUND_BIN):
 	mkdir -p `dirname $@`
 	curl -fsSL https://downloads.d2iq.com/dkp-bloodhound/dkp-bloodhound_v$(DKP_BLOODHOUND_VERSION)_$(GOOS)_$(GOARCH).tar.gz | tar xz -O > $@
@@ -13,5 +15,6 @@ validate-manifests: $(DKP_BLOODHOUND_BIN)
 	$(DKP_BLOODHOUND_BIN)
 
 .PHONY: list-images
+list-images: _SKIP_APPLICATIONS_FLAG := $(if $(SKIP_APPLICATIONS),--skip-applications $(SKIP_APPLICATIONS),)
 list-images: $(DKP_BLOODHOUND_BIN)
-	$(DKP_BLOODHOUND_BIN) --no-validation --list-artifacts --output-artifacts-file $(REPO_ROOT)/images.yaml --skip-applications ai-navigator-app,ai-navigator-cluster-info-agent,nkp-pulse-management,nkp-pulse-workspace
+	$(DKP_BLOODHOUND_BIN) --no-validation --list-artifacts --output-artifacts-file $(REPO_ROOT)/images.yaml $(_SKIP_APPLICATIONS_FLAG)
