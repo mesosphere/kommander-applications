b89cb55fc934a8493375d03f3ce19bc5426ab698
shubham2g <shubham.gupta2@nutanix.com>
2025-07-24 23:08:30 +0530

    feat: Add support for creating full Kommander application bundles

--- make/validate.mk
+++ make/validate.mk
@@ -5,8 +5,6 @@ GOARCH := $(shell go env GOARCH)
 
 SKIP_APPLICATIONS ?= ai-navigator-app,ai-navigator-cluster-info-agent,nkp-pulse-management,nkp-pulse-workspace
 
-YQ_SKIP_APPLICATIONS := $(shell echo $(SKIP_APPLICATIONS) | tr ',' '\n' | awk '{ printf "%s.name == \"%s\"", (NR==1?"":" or "), $$0 }')
-
 $(DKP_BLOODHOUND_BIN):
 	mkdir -p `dirname $@`
 	curl -fsSL https://downloads.d2iq.com/dkp-bloodhound/dkp-bloodhound_v$(DKP_BLOODHOUND_VERSION)_$(GOOS)_$(GOARCH).tar.gz | tar xz -O > $@
@@ -17,11 +15,9 @@ list-images: _SKIP_APPLICATIONS_FLAG := $(if $(SKIP_APPLICATIONS),--skip-applica
 list-images: $(DKP_BLOODHOUND_BIN)
 	$(DKP_BLOODHOUND_BIN) --no-validation --list-artifacts --output-artifacts-file $(REPO_ROOT)/images.yaml $(_SKIP_APPLICATIONS_FLAG)
 
-.PHONY: list-images-excluded-from-airgapped
-list-images-excluded-from-airgapped: $(DKP_BLOODHOUND_BIN)
-	$(DKP_BLOODHOUND_BIN) --no-validation --list-artifacts --output-artifacts-file $(REPO_ROOT)/_tmp_all_images.yaml
-	yq '.applications |= map(select($(YQ_SKIP_APPLICATIONS)))' $(REPO_ROOT)/_tmp_all_images.yaml > $(REPO_ROOT)/images-excluded-from-airgapped.yaml
-	rm $(REPO_ROOT)/_tmp_all_images.yaml
+.PHONY: list-images-full
+list-images-full: $(DKP_BLOODHOUND_BIN)
+	$(DKP_BLOODHOUND_BIN) --no-validation --list-artifacts --output-artifacts-file $(REPO_ROOT)/all_images.yaml
 
 # TODO : update with stable version once available
 NKP_CLI_VERSION := 2.16.0-dev.11
