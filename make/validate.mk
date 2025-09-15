84e4917bcd5e19984f84f49cd00eb07c18667edf
Tarun Gupta Akirala <tarun.akirala@nutanix.com>
2025-09-15 14:16:33 -0700

    ci: drop manifest validation check
    license check already runs the same validation
    
    Signed-off-by: Tarun Gupta Akirala <tarun.akirala@nutanix.com>

--- make/validate.mk
+++ make/validate.mk
@@ -8,7 +8,3 @@ list-images: $(NKP_CLI_BIN) $(YQ_BIN) list-images-full
 .PHONY: list-images-full
 list-images-full: $(NKP_CLI_BIN)
 	$(NKP_CLI_BIN) validate catalog-repository --repo-dir $(REPO_ROOT) --config $(REPO_ROOT)/.bloodhound.yml --artifacts-output $(REPO_ROOT)/images.yaml
-
-.PHONY: validate-manifests
-validate-manifests: $(NKP_CLI_BIN)
-	$(NKP_CLI_BIN) validate catalog-repository -v=3 --repo-dir=$(CURDIR) --config $(REPO_ROOT)/.bloodhound.yml
