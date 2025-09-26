4eb75bc8a41fb3e966ca40973979a187f4cfe401
Tarun Gupta Akirala <tarun.akirala@nutanix.com>
2025-09-25 21:07:58 -0700

    fix: adjust scripts
    Signed-off-by: Tarun Gupta Akirala <tarun.akirala@nutanix.com>

--- make/validate.mk
+++ make/validate.mk
@@ -4,6 +4,7 @@ SKIP_APPLICATIONS ?= ai-navigator-rag,ai-navigator-app,ai-navigator-cluster-info
 list-images: $(NKP_CLI_BIN) $(YQ_BIN) list-images-full
 	echo "Removing applications from images.yaml: $(SKIP_APPLICATIONS)"
 	yq eval 'del(.applications[] | select(.name as $$name | "$(SKIP_APPLICATIONS)" | split(",") | contains([$$name])))' -i $(REPO_ROOT)/images.yaml
+	yq '.applications[].images[]' images.yaml | sort | uniq | grep -v "oci://" > images.txt
 
 .PHONY: list-images-full
 list-images-full: $(NKP_CLI_BIN)
