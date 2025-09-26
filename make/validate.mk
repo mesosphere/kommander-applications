0f9a553a23fb4480b7481a99cfe62c470ff1ea50
Tarun Gupta Akirala <tarun.akirala@nutanix.com>
2025-09-25 21:37:32 -0700

    ci: simplify gha workflow file
    Signed-off-by: Tarun Gupta Akirala <tarun.akirala@nutanix.com>

--- make/validate.mk
+++ make/validate.mk
@@ -4,8 +4,14 @@ SKIP_APPLICATIONS ?= ai-navigator-rag,ai-navigator-app,ai-navigator-cluster-info
 list-images: $(NKP_CLI_BIN) $(YQ_BIN) list-images-full
 	echo "Removing applications from images.yaml: $(SKIP_APPLICATIONS)"
 	yq eval 'del(.applications[] | select(.name as $$name | "$(SKIP_APPLICATIONS)" | split(",") | contains([$$name])))' -i $(REPO_ROOT)/images.yaml
-	yq '.applications[].images[]' images.yaml | sort | uniq | grep -v "oci://" > images.txt
+	echo "Images after removing applications:"
+	cat $(REPO_ROOT)/images.yaml
+	yq '.applications[].images[]' $(REPO_ROOT)/images.yaml | sort | uniq | grep -v "oci://" > images.txt
+	echo "Final list of images in images.txt:"
+	cat images.txt
 
 .PHONY: list-images-full
 list-images-full: $(NKP_CLI_BIN)
 	$(NKP_CLI_BIN) validate catalog-repository --repo-dir $(REPO_ROOT) --config $(REPO_ROOT)/.bloodhound.yml --artifacts-output $(REPO_ROOT)/images.yaml
+	echo "Generated images.yaml:"
+	cat $(REPO_ROOT)/images.yaml
