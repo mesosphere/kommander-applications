75ba71250e1fc5f56f5c33000f303f2e1ee4e502
Tarun Gupta Akirala <tarun.akirala@nutanix.com>
2025-09-25 22:42:35 -0700

    ci: fix script
    Signed-off-by: Tarun Gupta Akirala <tarun.akirala@nutanix.com>

--- make/validate.mk
+++ make/validate.mk
@@ -1,9 +1,13 @@
 SKIP_APPLICATIONS ?= ai-navigator-rag,ai-navigator-app,ai-navigator-cluster-info-agent,nkp-pulse-management,nkp-pulse-workspace
 
 .PHONY: list-images
-list-images: $(NKP_CLI_BIN) $(YQ_BIN) list-images-full
+list-images: $(NKP_CLI_BIN) $(YQ_BIN) #list-images-full
 	echo "Removing applications from images.yaml: $(SKIP_APPLICATIONS)"
-	yq eval 'del(.applications[] | select(.name as $$name | "$(SKIP_APPLICATIONS)" | split(",") | contains([$$name])))' -i $(REPO_ROOT)/images.yaml
+	@if [ -n "$(SKIP_APPLICATIONS)" ]; then \
+		for app in $$(echo "$(SKIP_APPLICATIONS)" | tr ',' ' '); do \
+			yq eval "del(.applications[] | select(.name == \"$$app\"))" -i $(REPO_ROOT)/images.yaml; \
+		done; \
+	fi
 	echo "Images after removing applications:"
 	cat $(REPO_ROOT)/images.yaml
 	yq '.applications[].images[]' $(REPO_ROOT)/images.yaml | sort | uniq | grep -v "oci://" > images.txt
