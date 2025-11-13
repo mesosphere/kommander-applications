bb291e34139e7e236a9fa122c5390c7271863a7a
suresh882710 <suresh.lalge@nutanix.com>
2025-11-13 17:31:37 +0530

    feat: bump kubectl version v1.34.1-alpine (#4260)
    * feat: bump kubectl version v1.34.1-alpine
    
    * feat: bump kubectl version v1.34.1-alpine
    
    * feat: upgrade git-operator
    
    * feat: update image for git-operator
    
    * feat: update image for git-operator
    
    * feat: updated kubefed kubectl image repository
    
    * fix: skip upgrading crds
    
    * feat: updated lice
    
    * feat: updated lice
    
    ---------
    
    Co-authored-by: kshitij-kumar-ngt <kshitij.kumar@nutanix.com>

--- apptests/appscenarios/constants.go
+++ apptests/appscenarios/constants.go
@@ -20,6 +20,6 @@ const (
 	dkpCriticalPriority           = "dkp-critical-priority"
 
 	// Velero constants
-	kubetoolsImageRepository = "bitnamilegacy/kubectl:1.33.2"
-	kubetoolsImageTag        = "1.33.2"
+	kubetoolsImageRepository = "docker.io/mesosphere/kubectl:v1.34.1-alpine"
+	kubetoolsImageTag        = "v1.34.1-alpine"
 )
