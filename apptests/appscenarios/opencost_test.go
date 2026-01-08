a3aa7da773f13644fa0eb6cff156ed4f086efe8c
York Chen <york.chen@nutanix.com>
2026-01-08 13:20:32 -0500

    test: support multicluster in app tests (#4426)
    * test: support multicluster
    
    * test: rest client for both clusters
    
    * chore: cleanup
    
    * refactor: use existing Env struct
    
    * refactor: optional arg to use workload client
    
    * docs: fix the docker VM command
    
    * chore: remove redundant variable

---
+++ apptests/appscenarios/opencost_test.go
@@ -0,0 +1,46 @@
+package appscenarios
+
+import (
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/mesosphere/kommander-applications/apptests/environment"
+)
+
+var _ = Describe("Multi-Cluster OpenCost Tests", Label("opencost"), func() {
+	BeforeEach(OncePerOrdered, func() {
+		err := SetupMultiCluster()
+		Expect(err).To(Not(HaveOccurred()))
+
+		err = multiEnv.InstallLatestFlux(ctx)
+		Expect(err).To(Not(HaveOccurred()))
+
+		err = multiEnv.InstallLatestFluxOnWorkload(ctx)
+		Expect(err).To(Not(HaveOccurred()))
+
+		err = multiEnv.ApplyKommanderPriorityClasses(ctx, environment.ManagementClusterTarget)
+		Expect(err).To(Not(HaveOccurred()))
+
+		err = multiEnv.ApplyKommanderPriorityClasses(ctx, environment.WorkloadClusterTarget)
+		Expect(err).To(Not(HaveOccurred()))
+	})
+
+	AfterEach(OncePerOrdered, func() {
+		if os.Getenv("SKIP_CLUSTER_TEARDOWN") != "" {
+			return
+		}
+
+		err := TeardownMultiCluster()
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Describe("test", Ordered, Label("install"), func() {
+		It("should setup multi-cluster environment", func() {
+			Expect(multiEnv).ToNot(BeNil())
+			Expect(k8sClient).ToNot(BeNil())
+			Expect(workloadK8sClient).ToNot(BeNil())
+		})
+	})
+})
